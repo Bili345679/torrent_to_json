0c9ca3c53957e6b703662e9ce20682ffb0e57350
Bili345679 <3130759140@qq.com>
2024-12-12 21:56:01 +0800

    修复种子的name过长的bug，替换为torrent_info.info.name.utf-8

--- torrent_to_json.py
+++ torrent_to_json.py
@@ -7,13 +7,17 @@ prog_dir_name = os.path.dirname(sys.argv[0])
 if len(sys.argv) < 2:
     os.system("pause")
     exit()
-    
+
 file_paths = sys.argv[1:]
 for each in file_paths:
     if os.path.splitext(each)[1] == ".torrent" and os.path.isfile(each):
         torrent_info = tr.read_file(each)
-        name = torrent_info["info"]["name"]
+        if "name.utf-8" in torrent_info["info"]:
+            name = torrent_info["info"]["name.utf-8"]
+        else:
+            name = torrent_info["info"]["name"]
         json_path = f"{prog_dir_name}\\torrent_json\\{name}.json"
+
         real_file_path = common.json_dump(
             torrent_info,
             json_path,
